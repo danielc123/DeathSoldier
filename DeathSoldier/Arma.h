2dafee7b06e7d020dba8411f5d8132b614b4f3d3
cice <a>
2017-01-27 20:38:09 +0100

    Creada una clase arma

---
+++ DeathSoldier/Arma.h
@@ -0,0 +1,13 @@
+//
+//  Arma.h
+//  DeathSoldier
+//
+//  Created by cice on 27/1/17.
+//  Copyright © 2017 cice. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Arma : NSObject
+
+@end
---
+++ DeathSoldier/Arma.m
@@ -0,0 +1,13 @@
+//
+//  Arma.m
+//  DeathSoldier
+//
+//  Created by cice on 27/1/17.
+//  Copyright © 2017 cice. All rights reserved.
+//
+
+#import "Arma.h"
+
+@implementation Arma
+
+@end
