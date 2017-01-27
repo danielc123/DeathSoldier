b288e63cf171b00dc1915469ed6eb298bfb29e62
cice <a>
2017-01-27 20:15:33 +0100

    Initial Commit

---
+++ DeathSoldier/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DeathSoldier
+//
+//  Created by cice on 27/1/17.
+//  Copyright © 2017 cice. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
